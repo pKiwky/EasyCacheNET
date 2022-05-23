32a3aec268e9c882c311e9a84b182cba6ca7fef5
pKiwky <Picuandreia2001@gmail.com>
2022-05-23 17:09:33 +0300

    Folders

--- EasyCacheNET/src/Utility.cs
+++ EasyCacheNET/src/Utility.cs
@@ -4,7 +4,6 @@ namespace EasyCacheNET {
 
 	public static class Utility {
 		public static DateTimeOffset IncrementDate(TimeSpan duration) {
-			// TODO: Safe return.
 			return DateTimeOffset.UtcNow.AddSeconds(duration.TotalSeconds);
 		}
 	}
